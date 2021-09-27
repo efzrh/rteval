0633fb6bfa4e344fb13dd234213cbb9989bf86a5
John Kacur <jkacur@redhat.com>
2021-09-27 16:39:05 -0400

    rteval: systopology: Update copyright
    Update the copyright
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- rteval/systopology.py
+++ rteval/systopology.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #   Copyright 2016 - Clark Williams <williams@redhat.com>
+#   Copyright 2021 - John Kacur <jkacur@redhat.com>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
